9ab44ad55c723c27f933eac3266078faf644bbf8
Ait idir Ferial <100928322+FerialAit@users.noreply.github.com>
2022-06-23 09:55:43 +0200

    exos

---
+++ 09_boutique/inc/test.php
@@ -0,0 +1,27 @@
+<?php
+$host = 'localhost';
+$database = 'site';
+$user = 'root';
+$psw = '';
+
+$pdoSITE = new PDO('mysql:host='.$host.';dbname='.$database, $user, $psw);
+$pdoSITE -> exec('SET NAMES utf8');
+// echo 'coucou';
+$requete = $pdoSITE->query("SELECT * FROM membre");
+$ligne = $requete->fetch(PDO::FETCH_ASSOC);
+
+require_once('function.php');
+jeprint_r($requete);
+echo "<p>Les infos de notre premier membre : </p>";
+echo "<ul>";
+echo "<li>Prénom : " .$ligne['prenom']. "</li><li>Nom : ".$ligne['nom']. "</li><li>Pseudo : " .$ligne['pseudo']. "</li><li>Adresse : " .$ligne['adresse']. "</li><li>Code postal : " .$ligne['code_postal']. "</li><li>Sexe : " ;
+if ($ligne['civilite'] == 'f'){
+    echo 'Féminin';
+
+}else {
+    echo "masculin";
+
+}
+echo "</li>";
+echo "</ul>";
+?>
